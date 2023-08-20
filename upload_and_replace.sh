04ec179108a77693e95aaacf9a84419e85f10c67
fliaping <269832017@qq.com>
2023-08-21 00:16:17 +0800

    add

--- upload_and_replace.sh
+++ upload_and_replace.sh
@@ -21,21 +21,31 @@ function upload_images_and_get_links() {
   mkdir -p "./temp_$uploads_dir/$date_part/$time_part"
 
   # 将'uploads_dir'移动到时间子目录
-  cp "./$uploads_dir/**" "./temp_$uploads_dir/$date_part/$time_part"
+  cp -r "./$uploads_dir/." "./temp_$uploads_dir/$date_part/$time_part/"
 
-  # qshell qupload2 --bucket xpghost --src-dir "./temp_$uploads_dir" --overwrite --thread-count 5 --rescan-local
+  qshell qupload2 --bucket xpghost --src-dir "./temp_$uploads_dir" --overwrite --thread-count 5 --rescan-local
   
-  # # Recursively upload and get public links for each file in the 'images' directory
-  # for file_path in $(find "$uploads_dir" -type f); do
-  #   local public_link="https://storage.blog.fliaping.com/$date_part/$time_part${file_path#$uploads_dir}"
-  #   echo "$file_path,$public_link" > "$public_links_file"
-  # done
+  # Recursively upload and get public links for each file in the 'images' directory
+  for file_path in $(find "$uploads_dir" -type f); do
+    local public_link="https://storage.blog.fliaping.com/$date_part/$time_part${file_path#$uploads_dir}"
+    echo "$file_path,$public_link" >> "$public_links_file"
+  done
 
-  # echo "Step 1: Upload and get public links for '$uploads_dir' - Done!"
+  echo "Step 1: Upload and get public links for '$uploads_dir' - Done!"
 }
 
 regex_escape_path() {
     echo "$1" | sed -e 's/[]\/$*.^[]/\\&/g'
+}
+
+escape_path() {
+    local path="$1"
+    # 使用反斜线转义特殊字符
+    echo "$path" | sed -e 's/\*/\\*/g' \
+                       -e 's/\$/\\$/g' \
+                       -e 's/ /\\ /g' \
+                       -e 's/\[/\\[/g' \
+                       -e 's/\]/\\]/g'
 }
 
 # Step 2: Replace local image paths with public links in markdown files
@@ -63,6 +73,7 @@ function replace_image_paths_in_markdown() {
 
   # Recursively search and replace local image paths with public links in markdown files
   for file_path in $(find "$content_dir" -type f -name "*.md"); do
+    file_path=$(escape_path "$file_path")
     # 获取子目录深度
     depth=$(echo "$file_path" | tr -cd '/' | wc -c)
     
@@ -73,8 +84,7 @@ function replace_image_paths_in_markdown() {
     for index in "${!local_paths[@]}"; do
       local local_path="${local_paths[$index]}"
       local public_link="${public_links[$index]}"
-      local path_in_md="$back_to_root$local_path"
-      echo 
+      local path_in_md="$back_to_root$local_path" 
 
       # 使用 uname 检测操作系统
       OS=$(uname)
@@ -83,7 +93,6 @@ function replace_image_paths_in_markdown() {
       escaped_path=$(regex_escape_path "$path_in_md")
       m_pattern="!\\[.*\\]\\($escaped_path\\)"
       line_numbers=$(awk -v pattern="$m_pattern" 'match($0, pattern) { print NR }' "$file_path")
-      echo "$line_numbers"
 
       # 如果有匹配的行，输出完整的行和行号
       if [ ! -z "$line_numbers" ]; then
@@ -126,5 +135,5 @@ function clean_cache_dir() {
 
 # Main execution
 upload_images_and_get_links
-# replace_image_paths_in_markdown
-# clean_cache_dir
+replace_image_paths_in_markdown
+clean_cache_dir
